5140d33002a017f78aa609d3b70a858d5aec6bbf
Allan Guedes <allan-g8@hotmail.com>
2018-10-23 00:06:02 -0300

    Para Estudos

---
+++ unidade_04/inicial.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+         <p><?php echo "Allan Guedes" ?></p>
+         <p><?php echo 2+2 ?></p>
+    </body>
+</html>
